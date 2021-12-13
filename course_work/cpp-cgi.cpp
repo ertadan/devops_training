19b14a8a3c29f637abc6c0e6ae95634ffec6334e
Alex Lem <vin021110@gmail.com>
2021-12-13 22:31:00 +0200

    add course files

---
+++ course_work/cpp-cgi.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+int main () {
+
+    using std::cout;
+   //generate random number
+   srand((unsigned) time(0));
+   cout << "Content-type:text/html\r\n\r\n";
+   cout << "<html>\n";
+   cout << "<head>\n";
+   cout << "<title>Hello World - CGI Program</title>\n";
+   cout << "</head>\n";
+   cout << "<body>\n";
+   cout << "<h2>Hello ! This is my first CGI program</h2>\n";
+   cout << "<br> It can generate random numbers as: <u> " <<
+	   rand()  << " </u>\n";
+   cout << "</body>\n";
+   cout << "</html>\n";
+   
+   return 0;
+}
---
+++ lesson7/ec2.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "ertadan/lesson7"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "ertadan"
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "lesson7" {
+  ami           = "ami-03e0722760e96e886"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.deployer.key_name
+  subnet_id = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.SG_public_subnet.id]
+  tags = {
+    Name = "lesson7-instance"
+  }
+}
+
